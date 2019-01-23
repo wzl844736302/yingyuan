9f141f6369dfe70b0ab40f6a8e47369e844e3603
wzl844736302 <844736302@qq.com>
2019-01-23 11:19:20 +0800

    引导页1

--- dimensionscinema/src/main/java/com/bw/movie/frag/Frag_four.java
+++ dimensionscinema/src/main/java/com/bw/movie/frag/Frag_four.java
@@ -1,5 +1,6 @@
 package com.bw.movie.frag;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,14 +8,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.bw.movie.R;
+import com.bw.movie.view.LoginActivity;
 
-public class Frag_four extends Fragment {
+public class Frag_four extends Fragment implements View.OnClickListener {
+
+    private RelativeLayout mrelate;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag_four,container,false);
+        //初始化控件
+        mrelate = view.findViewById(R.id.mRelate);
+        mrelate.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        //点击跳转
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        startActivity(intent);
+        getActivity().finish();
     }
 }
--- dimensionscinema/src/main/java/com/bw/movie/view/GuideActivity.java
+++ dimensionscinema/src/main/java/com/bw/movie/view/GuideActivity.java
@@ -1,11 +1,16 @@
 package com.bw.movie.view;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.bw.movie.R;
 import com.bw.movie.adapter.GuideAdapter;
@@ -25,21 +30,55 @@ public class GuideActivity extends AppCompatActivity implements CustomAdapt {
 
     @BindView(R.id.mView)
     ViewPager mview;
+    @BindView(R.id.mRadio)
+    RadioGroup mradio;
     private List<Fragment> list = new ArrayList<>();
+    private SharedPreferences sp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
         //绑定
         ButterKnife.bind(this);
+        sp = getSharedPreferences("welcome", Context.MODE_PRIVATE);
         //添加到list里面
         list.add(new Frag_one());
         list.add(new Frag_two());
         list.add(new Frag_three());
         list.add(new Frag_four());
         //设置适配器
-        GuideAdapter adapter = new GuideAdapter(getSupportFragmentManager(),list);
+        GuideAdapter adapter = new GuideAdapter(getSupportFragmentManager(), list);
         mview.setAdapter(adapter);
+        //默认选中第一个
+        mradio.check(mradio.getChildAt(0).getId());
+        //联动
+        mview.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                mradio.check(mradio.getChildAt(i).getId());
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+        //判断
+        if (sp.getBoolean("bool", false)) {
+            Intent intent = new Intent(GuideActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        //启动页判断是否第一次启动
+        SharedPreferences.Editor edit = sp.edit();
+        edit.putBoolean("bool", true);
+        edit.commit();
     }
 
     @Override
