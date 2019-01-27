56843a69cfb84f38169f5f372cd71c0605058e86
wzl844736302 <844736302@qq.com>
2019-01-27 20:29:50 +0800

    传值1

--- dimensionscinema/src/main/java/com/bw/movie/frag/FragMovie.java
+++ dimensionscinema/src/main/java/com/bw/movie/frag/FragMovie.java
@@ -220,6 +220,10 @@ public class FragMovie extends Fragment implements HotMovieAdapter.onItemClick,V
     public void et_sou(){
         et_sou.setVisibility(View.GONE);
         tv_sou.setVisibility(View.GONE);
+
+
+
+
     }
     //热门电影
     private class HorMovieData implements DataCall<Result<List<HotMovie>>> {
